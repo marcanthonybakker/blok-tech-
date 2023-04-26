f7a152d29de173b0404e34efa203125aa557efd4
marcanthonybakker <marc.bakker2@hva.nl>
2023-04-26 08:38:06 +0200

    server opdracht

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello wordl!')
+const express = require('express')
+
+express()
+    .get('/', onhome)
+    .get('/about', onabout)
+
+    .listen(8000)
+
+function onhome (req, res) {
+    res.send('<h1>Hello World!</h1>')
+}
+
+function onabout(req, res) {
+    res.send('<h1>About me</h1>')
+}
