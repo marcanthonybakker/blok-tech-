db8faad6ec5269ca0a26dfee219bd504522928a8
marcanthonybakker <marc.bakker2@hva.nl>
2023-05-01 11:18:59 +0200

    w2 back end opdracht

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require('express')
+const app = express();
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
 
 express()
     .get('/', onhome)
     .get('/about', onabout)
-
+    .get('/login', onlogin)
+    .use(onNotFound)
     .listen(8000)
 
-function onhome (req, res) {
+function onhome(req, res) {
     res.send('<h1>Hello World!</h1>')
+    // app.get('/', (req, res) => {
+    //     const data = {
+    //       pageTitle: 'My Website',
+    //       currentDate: new Date().toDateString()
+    //     };
+    //     res.render('index', data); // Render the index.ejs template with the provided data
+    //   });
 }
 
 function onabout(req, res) {
     res.send('<h1>About me</h1>')
+}
+
+function onlogin(req, res) {
+    res.send('<h1>Login</h1>')
+}
+
+function onNotFound(req, res) {
+    res.status(404).send('<h1>Error 404: Page not found</h1>');
 }
