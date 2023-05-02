9b7c349d0110f4e3454d94b6b74cd58e139ffee4
marcanthonybakker <marc.bakker2@hva.nl>
2023-05-02 13:02:40 +0200

    FE opdracht w2

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log("Hello world");
+
+const form = document.getElementById('form');
+
+// function valideerPlaats() {
+//     const plaats  = document.getElementById('form section:nth-last-of-type(2) label');
+
+//     let isValid = true;
+
+//     if (plaats === 'Purmerend') {
+//         isValid = false;
+//         console.log("Plaats fout")
+//     } else {
+//         console.log("Plaats correct")
+//     }
+// }
