fc9dd2c7f19ad404d87aabc740ecee0608299699
marcanthonybakker <marc.bakker2@hva.nl>
2023-05-12 09:36:09 +0200

    toevoeging header ejs en aanpassingen

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+// Server express en ejs laten gebruiken
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
 
+// Templating engine aanzetten met statische content
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-app.use(express.static('public'));
+app.use(express.static('static'));
 
+// Formulier pagina https link
 app.get('/', (req, res) => {
-  res.render('aap');
+  res.render('form');
 });
 
+// Server open zetten op port 3000
 app.listen(3000, () => {
-  console.log("Whats up! The server has started on port 3000. Have fun!");
+  console.log("Server aan op port 3000");
 });
