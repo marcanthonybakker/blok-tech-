03e3ff1d33cf41b6eaa7379378d1c2094366e5fe
marcanthonybakker <marc.bakker2@hva.nl>
2023-05-12 08:34:14 +0200

    ejs gelinkt

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const ejs = require('ejs');
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.static('public'));
 
-express()
-  .get("/", onhome)
-  .get("/about", onabout)
-  .get("/login", onlogin)
-  .use(onNotFound)
-  .listen(8000);
+app.get('/', (req, res) => {
+  res.render('aap');
+});
 
-function onhome(req, res) {
-  res.send("<h1>Hello World!</h1>");
-  // app.get('/', (req, res) => {
-  //     const data = {
-  //       pageTitle: 'My Website',
-  //       currentDate: new Date().toDateString()
-  //     };
-  //     res.render('index', data); // Render the index.ejs template with the provided data
-  //   });
-}
-
-function onabout(req, res) {
-  res.send("<h1>About me</h1>");
-}
-
-function onlogin(req, res) {
-  res.send("<h1>Login</h1>");
-}
-
-function onNotFound(req, res) {
-  res.status(404).send("<h1>Error 404: Page not found</h1>");
-}
+app.listen(3000, () => {
+  console.log("Whats up! The server has started on port 3000. Have fun!");
+});
