2f94832f249951d06a4a9c424a2ca87fbc53a85f
marcanthonybakker <marc.bakker2@hva.nl>
2023-05-13 13:05:02 +0200

    js prog. enh. toegevoegd

--- static/js/script.js
+++ static/js/script.js
@@ -3,10 +3,60 @@ console.log("script.js geladen");
 
 // Event listeners voor het formulier
 
-const inputPlaats = document.querySelector(".inputPlaats"); 
+const submitButton = document.querySelector('.form main div:last-of-type>input');
 
-console.log(inputPlaats);
+const inputHobby = document.getElementById('hobby-select');
+const hobbySelector = document.querySelector('.form select');
+let hobbyAan = false;
 
-inputPlaats.addEventListener('click', () => {
-    console.log("yes");
-});
+const inputPlaats = document.getElementById('plaats');
+const plaatsSelector = document.querySelector('.form input');
+let plaatsAan = false;
+
+const inputDatum = document.getElementById('datum');
+const datumSelector = document.querySelector('.form input[type=date]');
+let datumsAan = false;
+
+const inputBeschrijving = document.getElementById('beschrijving');
+const beschrijvingSelector = document.querySelector('.form main textarea');
+let beschrijvingAan = false;
+
+if (hobbyAan == false && plaatsAan == false && datumsAan == false && beschrijvingAan == false) {
+  console.log("submit uit");
+  submitButton.classList.add('uit');
+}
+
+function checkSubmitButton() {
+    if (hobbyAan && plaatsAan && datumsAan && beschrijvingAan) {
+      console.log("submit uit");
+      submitButton.classList.remove('uit');
+    }
+  }
+  
+  inputHobby.addEventListener('focus', () => {
+    console.log("hobby gefocust");
+    hobbySelector.classList.add('ingevuld')
+    hobbyAan = true;
+    checkSubmitButton();
+  });
+  
+  inputPlaats.addEventListener('focus', () => {
+    console.log("plaats gefocust");
+    plaatsSelector.classList.add('ingevuld')
+    plaatsAan = true;
+    checkSubmitButton();
+  });
+  
+  inputDatum.addEventListener('focus', () => {
+    console.log("datum gefocust");
+    datumSelector.classList.add('ingevuld')
+    datumsAan = true;
+    checkSubmitButton();
+  });
+  
+  inputBeschrijving.addEventListener('focus', () => {
+    console.log("beschrijving gefocust");
+    beschrijvingSelector.classList.add('ingevuld')
+    beschrijvingAan = true;
+    checkSubmitButton();
+  });
