1f573a04c84ef22652a8cb1f2db6c4b7b0d63a64
marcanthonybakker <marc.bakker2@hva.nl>
2023-05-29 16:36:10 +0200

    Voortgang mongoDB Gebruikersnaam en verstuur en meer

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // // Haal wachtwoorden en andere data uit de .env file
 require("dotenv").config();
 
+//-
+
 // MongoDB connectie test ruimte
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = process.env.DB_PASSWORD;
+
+//-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 console.log("uri: ", uri);
@@ -29,20 +33,38 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
+//-
 
 // Server express en ejs laten gebruiken
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
 
-// Gebruiker data (nep data omdat ik nog geen database heb)
-const gebruikerData = "Marc Bakker (server data)";
+// collectie van de database aanroepen
+const collectionGebruikerData = client.db("bloktech").collection("gebruikersNaam");
+const collectionFormulierData = client.db("bloktech").collection("formulierDataCollectie");
+
+//-
+
+// Formulier gegevens naar de database sturen
+async function insertDataToDatabase(data) {
+  try {
+    // Verbind met de database
+    await client.connect();
+
+    // Selecteer de gewenste database en collectie
+    const db = client.db("bloktech");
+
+    // Voeg de gegevens toe aan de collectie
+    const result = await collectionFormulierData.insertOne(data);
+    console.log("Gegevens zijn succesvol toegevoegd aan de database:", result.insertedId);
+  } catch (err) {
+    console.error("Fout bij het toevoegen van gegevens aan de database:", err);
+  } finally {
+    // Sluit de verbinding met de database
+    await client.close();
+  }
+}
 
 // Variabelen om geposte gegevens op te slaan
 let plaatsData = "";
@@ -50,39 +72,84 @@ let hobbyData = "";
 let datumData = "";
 let beschrijvingData = "";
 
+//-
+
 // Templating engine aanzetten met statische content en geposte data ophalen
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static("static"));
 app.use(express.urlencoded({ extended: true }));
 
+//-
+
 // Formulier pagina https link
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const gebruikerData = await collectionGebruikerData.find({}).toArray();
   res.render("form.ejs", { gebruikerData });
 });
 
-app.post('/berichten', function(req, res) {
+//-
+
+// Formulier gegevens ophalen en doorsturen naar de database
+app.post('/berichten', async function(req, res) {
   plaatsData = req.body.plaats;
   hobbyData = req.body.hobbys;
   datumData = req.body['trip-start'];
   beschrijvingData = req.body.beschrijving;
 
-  // Hier kun je de gegevens verder verwerken, bijvoorbeeld opslaan in een database of uitvoeren van andere logica
-
-  // Doorsturen naar /berichten
-  res.redirect("/berichten");
-});
-
-// Berichten pagina https link
-app.get("/berichten", (req, res) => {
-  res.render("berichten.ejs", { 
+  // Gegevens opslaan in een object
+  const formulierData = {
     plaatsData,
     hobbyData,
     datumData,
-    beschrijvingData,
-    gebruikerData
-  });
+    beschrijvingData
+  };
+
+  try {
+    // Gegevens naar de database sturen
+    await insertDataToDatabase(formulierData);
+
+    // Doorsturen naar /berichten
+    res.redirect("/berichten");
+  } catch (err) {
+    console.error("Fout bij het verwerken van gegevens:", err);
+    // Stuur een foutmelding naar de gebruiker
+    res.render("error.ejs");
+  }
 });
+
+//-
+
+// Berichten pagina https link
+app.get("/berichten", async (req, res) => {
+  try {
+    const gebruikerData = await collectionGebruikerData.find({}).toArray();
+    res.render("berichten.ejs", { 
+      plaatsData,
+      hobbyData,
+      datumData,
+      beschrijvingData,
+      gebruikerData
+    });
+  } catch (err) {
+    console.error("Fout bij het ophalen van formulier data", err);
+  }
+});
+
+//-
+
+// Data ophalen uit de database oefening
+app.get("/data", async (req, res) => {
+  try {
+    const gebruikerData = await collectionGebruikerData.find({}).toArray();
+    const formulierData = await collectionFormulierData.find({}).toArray();
+    res.render("data.ejs", { gebruikerData, plaatsData });
+  } catch (err) {
+    console.error("Fout bij het ophalen van gegevens:", err);
+  }
+});
+
+//-
 
 // Error 404: ontbrekende pagina of onjuist pad
 app.get("/*", (req, res) => {
@@ -90,8 +157,9 @@ app.get("/*", (req, res) => {
   res.render("error.ejs");
 })
 
+//-
+
 // Server open zetten op poort 3000
 app.listen(3000, () => {
   console.log("Server draait op poort 3000");
 });
-
